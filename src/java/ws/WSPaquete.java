29082183108d497ae8d2f952c08f96cc8a6f32a4
Andres Flores Santamaria <43183282+AndresFS30@users.noreply.github.com>
2024-11-22 01:43:31 -0600

    Modificacion en Paquete

--- src/java/ws/WSPaquete.java
+++ src/java/ws/WSPaquete.java
@@ -5,10 +5,91 @@
  */
 package ws;
 
+import com.google.gson.Gson;
+import dominio.ImpColaborador;
+import dominio.ImpPaquete;
+import java.util.List;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import pojo.Colaborador;
+import pojo.Mensaje;
+import pojo.Paquete;
+
 /**
  *
  * @author afs30
  */
+@Path("paquete")
 public class WSPaquete {
+        @Context
+    private UriInfo context;
+   
+    public WSPaquete(){
+    }
+    
+    @Path("obtenerPaquete")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Paquete> obtenerPaquete(){
+      return ImpPaquete.obtenerPaquete();
+    }
+    
+    @GET
+    @Path("obtenerPaqueteEnvio/{IdEnvio}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Paquete>obtenerPaqueteEnvio(@PathParam("IdEnvio")Integer IdEnvio){
+        return ImpPaquete.obtenerPaqueteEnvio(IdEnvio);
+    }
+    
+    @Path("registrarPaquete")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Mensaje registrarPaquete(String jsonPaquete){
+        
+       try{
+       Gson gson = new Gson();
+        Paquete paquete = gson.fromJson(jsonPaquete, Paquete.class);
+        return ImpPaquete.registrarPaquete(paquete);
+       
+       }catch(Exception e){
+        e.printStackTrace();
+        throw new BadRequestException();
+       }
+        
+    }
+    
+    @Path("editarPaquete")
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Mensaje editarPaquete(String jsonPaquete){
+        try{
+        Gson gson = new Gson();
+        Paquete paquete = gson.fromJson(jsonPaquete, Paquete.class);
+        return ImpPaquete.editarPaquete(paquete);
+        }catch( Exception e){
+            e.printStackTrace();
+            throw new BadRequestException();
+        }
+    }
+    
+    @Path("eliminarPaquete")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public Mensaje eliminarPaquete(@QueryParam("IdPaquete") Integer IdPaquete) {
+        return ImpPaquete.eliminarPaquete(IdPaquete);
+    }
     
 }
